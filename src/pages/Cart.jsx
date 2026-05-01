3f900de61788876d4e29d4887e2ec7ad9e5442e6
marvin <mcanastuj1999@gmail.com>
2026-05-01 17:31:25 -0600

    place order and show order number and price added

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,14 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useCart } from "../context/CartContext"
 import { AuthContext } from "../context/AuthContext";
 import { supabase } from "../libs/supabaseClient";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 function Cart () {
     const { cart, removeFromCart, totalPrice,  clearCart, totalItems } = useCart() 
     const { user } = useContext(AuthContext)
+    const [isProcessing, setIsProcessing] = useState(false)
+    const navigate = useNavigate()
 
     const handleCheckout = async () => {
+        if (cart.length === 0 ) return toast.error("El carrito esta vacio")
+        setIsProcessing(true)
+        const toastId = toast.loading("Procesando tu compra") 
         try {
             const {data: order, error: orderError} = await supabase
                 .from("orders")
@@ -32,9 +38,13 @@ function Cart () {
             if(itemsError) throw itemsError
 
             await clearCart()
-            toast.success("Compra Realizada")
+            toast.success("Compra Realizada", {id: toastId})
+            navigate("/orders")
         } catch (error) {
-            toast.error("Error al procesar la compra")
+            toast.error("Error al procesar la compra", {id: toastId})
+            console.error(error)
+        } finally{
+            setIsProcessing(false)
         }
     }
 
@@ -69,7 +79,7 @@ function Cart () {
                     </tr>
                 </tfoot>
             </table>
-            <button className="bg-button px-8 py-3 rounded-2xl text-textbutton font-bold">Comprar</button>
+            <button onClick={handleCheckout} className="bg-button px-8 py-3 rounded-2xl text-textbutton font-bold">Comprar</button>
         </main>
     )
 }
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,17 +1,71 @@
+import { useContext, useEffect, useState } from "react"
+import { supabase } from "../libs/supabaseClient"
+import { AuthContext } from "../context/AuthContext"
+import toast from "react-hot-toast"
+
 function Orders () {
+    const [myOrders, setMyOrders] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const { user } = useContext(AuthContext)
+
+    const fetchOrders = async () => {
+        try {
+            setLoading(true)
+            const { data, error } = await supabase
+                .from("orders")
+                .select(`
+                    *,
+                    order_items (
+                        quantity,
+                        unit_price
+                    )
+                    `)
+                .eq("user_id", user.id)
+
+            if (error) throw error
+            setMyOrders(data)
+        } catch (error) {
+            toast.error("No pudimos cargar tus ordenes")
+            console.error(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        if (user) {
+            fetchOrders()
+        }
+    }, [user])
+
+    if (loading) return <p className="text-center my-20">Cargando tu historial...</p>
+
     return(
         <main className="px-5 xl:mx-[10%] my-10 flex flex-col items-center justify-center">
-            <h1 className="font-bold text-2xl">Tus Ordenes</h1>
-            <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5">
-                <article className="shadow-lg rounded-lg p-5 flex flex-col ">
-                    <div className="flex justify-between mb-5">
-                        <p className="font-bold text-lg ">Orden #<span>10</span></p>
-                        <p className="font-bold text-xs border-2 p-1 rounded-2xl text-red-700">Pendiente</p>
-                    </div>
-                    <p><span className="font-bold">Entregar a: </span><span className="font-semibold text-sm">Nombre Persona</span></p>
-                    <p><span className="font-bold">Direccion: </span><span className="font-semibold text-sm">Direccion</span></p>
-                    <p><span className="font-bold">Total: </span><span className="font-semibold text-sm">Q50.00</span></p>
-                </article>
+            <h1 className="font-bold text-2xl mb-10">Tus Ordenes</h1>
+            <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4  gap-5 w-full">
+                {myOrders.length === 0 ? (
+                    <p className="text-center my-20">No se ha realizado una orden anteriormente</p>
+                ): (
+                    myOrders.map((order) => {
+                        const items = order.order_items || []
+                        const orderTotal = items.reduce((acc, item) => {
+                            return acc + (Number(item.quantity || 0) * Number(item.unit_price || 0 ))
+                        }, 0)
+                        return(
+                            <article className="w-full shadow-lg rounded-lg p-5 flex flex-col " key={order.id}>
+                                <div className="flex justify-between items-center mb-5 gap-5">
+                                    <p className="font-bold text-lg ">Orden #<span>{order.id.toString().slice(0,5)}</span></p>
+                                    <p className={`${order.status === "pending" ? "text-red-700" : "text-green-700"} font-bold text-xs border-2 p-1 rounded-2xl `}>{order.status === "pending" ? "Pendiente" : "Completado"}</p>
+                                </div>
+                                <p><span className="font-bold">Entregar a: </span><span className="font-semibold text-sm">Nombre</span></p>
+                                <p><span className="font-bold">Direccion: </span><span className="font-semibold text-sm">Direccion</span></p>
+                                <p><span className="font-bold">Total: </span><span className="font-semibold text-sm">Q{orderTotal.toFixed(2)}</span></p>
+                            </article>
+                        )
+                    }))}
+                
             </section>
         </main>
     )
